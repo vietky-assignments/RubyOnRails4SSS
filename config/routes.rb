3d8e3f57ea9b604bac5876e01b41beea12ff19e1
Ky Quoc Viet <kq.viet@gmail.com>
2015-12-26 17:17:05 +0700

    Add: - Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
     resources :articles do
         resources :comments
     end
+    
+    get 'home', controller: 'home', action: 'index'
 
-    get 'home/index'
-    root 'home#index'
-
+    root 'articles#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
